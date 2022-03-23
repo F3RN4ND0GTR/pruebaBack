f01e084830f97aca81a484ab4e0b7ef6b0e0f653
F3RN4ND0GTR <fernando.gtr@gmail.com>
2022-03-23 10:26:17 +0100

    Fixes

--- src/main/java/com/example/calculator/controller/CalculatorController.java
+++ src/main/java/com/example/calculator/controller/CalculatorController.java
@@ -5,6 +5,8 @@ package com.example.calculator.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.calculator.service.CalculatorService;
 import com.example.calculator.utils.Constants;
@@ -19,6 +21,8 @@ public class CalculatorController {
 	@Autowired
 	public CalculatorService calculatorService;
 
+	@GetMapping("/operation")
+	@ResponseBody
 	public Integer operation(int valor1, int valor2, String operation) throws Exception {
 		if (Constants.SUMA.equals(operation)) {
 			return calculatorService.add(valor1, valor2);
